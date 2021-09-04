d5fbd40eed6ef832fec5d9b208eaff30003c84eb
ParshinAD <48247414+ParshinAD@users.noreply.github.com>
2021-09-04 22:54:42 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,55 @@
+﻿#pragma warning(disable : 4996)
+
+#define WINVER 0x0A00
+#include <iostream>
+#include <windows.h>
+#include <stdio.h>
+#include <tchar.h>
+
+using namespace std;
+
+#define BUFSIZE MAX_PATH
+#define FILESYSNAMEBUFSIZE MAX_PATH
+#define MAX_KEY_LENGTH 255
+
+
+#define INFO_BUFFER_SIZE 32767
+
+
+int main() {
+    DWORD dwSize;
+    TCHAR szName[MAX_KEY_LENGTH];
+    LPSTR lpValueName;
+    HKEY hKey;
+    DWORD dwIndex = 0;
+    DWORD retCode;
+    DWORD BufferSize = 8192;
+    PPERF_DATA_BLOCK PerfData = (PPERF_DATA_BLOCK)malloc(BufferSize);
+    DWORD cbData = BufferSize;
+
+    if (RegOpenKeyExA(HKEY_CURRENT_USER, "Software\\Microsoft\\Windows\\CurrentVersion\\Run",
+        0, KEY_ALL_ACCESS, &hKey) == !ERROR_SUCCESS)
+    {
+        printf("Function RegOpenKeyEx() failed!\n");
+    }
+    else printf("\n1.5.  Startup commands:\n");
+
+    while (1) {
+        dwSize = sizeof(szName);
+        retCode = RegEnumValue(hKey, dwIndex, szName, &dwSize, NULL, NULL, NULL, NULL);
+        if (retCode == ERROR_SUCCESS)
+        {
+            RegQueryValueEx(hKey, szName, NULL, NULL, (LPBYTE)PerfData, &cbData);
+            printf("      %d: %s:  %s\n", dwIndex + 1, szName, PerfData);
+            dwIndex++;
+        }
+        else break;
+    }
+
+    RegCloseKey(hKey);
+}
+
+
+
+
+
