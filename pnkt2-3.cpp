1153e7f2c8c5d71e526fe093ae1d648b00c0ea5b
ParshinAD <48247414+ParshinAD@users.noreply.github.com>
2021-09-05 11:51:56 +0300

    Update pnkt2-3.cpp

--- pnkt2-3.cpp
+++ pnkt2-3.cpp
@@ -1,8 +1,5 @@
-
 #include <windows.h>
-
 #include <stdio.h>
-
 #include <tchar.h>
 
 using namespace std;
@@ -10,18 +7,16 @@ using namespace std;
 #define INFO_BUFFER_SIZE 32767
 
 int main()
-
 {
 
     TCHAR szPath[_MAX_PATH] = { 0 };
     UINT iRet = GetSystemDirectory(szPath, _MAX_PATH);
-    printf("Result function GetSystemDirectory %d\n", iRet);
+    printf("Result %d\n", iRet);
 
     char buffer[256];
     DWORD size = 256;
     GetComputerNameA(buffer, &size);
     printf("%s\n", buffer);
-
 
     GetUserNameA(buffer, &size);
     printf("%s\n", buffer);
